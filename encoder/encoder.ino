f8adc168bc791451e9feba8a09b5a13b7614713d
spearkiller0 <abdohamdymetwaly@gmail.com>
2018-05-07 09:48:33 +0200

    main code & encoder pis are modified in the main code encoder folder used to determine the relation between voltage and the speed of motor ... by applying many voltages and measuring the speed we can git 1st order equtaion speed = m * volt

---
+++ encoder/encoder.ino
@@ -0,0 +1,88 @@
+#define EncoderR 2
+#define EncoderL 3
+
+#define ENA 9
+#define IN1 4
+#define IN2 5
+#define IN3 6
+#define IN4 7
+#define ENB 10
+
+#define T 2000
+#define PULSES_PER_TURN_L 30
+#define PULSES_PER_TURN_R 55
+
+#define R 0.031
+
+void EncoderRClicks();
+void EncoderLClicks();
+
+int PulseCountR, PulseCountL;
+
+void setup(){
+  Serial.begin(9600);
+
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  //intialize encoders' pin and attach the interupt
+  pinMode(EncoderR, INPUT);
+  pinMode(EncoderL, INPUT);
+  
+  digitalWrite(EncoderR, HIGH);
+  digitalWrite(EncoderL, HIGH);
+  attachInterrupt(digitalPinToInterrupt(EncoderR), EncoderRClicks, RISING);
+  attachInterrupt(digitalPinToInterrupt(EncoderL), EncoderLClicks, RISING);
+
+}
+
+void loop() {
+
+  double RPM_R = (PulseCountR * (60000.f / T)) / (PULSES_PER_TURN_R);
+  double RPM_L = (PulseCountL * (60000.f / T)) / (PULSES_PER_TURN_L);
+  PulseCountR = PulseCountL = 0;
+
+  double VR = 2*PI*R*RPM_R/60;
+  double VL = 2*PI*R*RPM_L/60;
+
+  double SlopeR = .0;
+  double SlopeL = .06;
+
+  int voltR = (int)(0.37/SlopeR);
+  voltR = map(voltR, 0, 7.92, 0, 220);
+  
+  int voltL = (int)(0.37/SlopeL);
+  voltL = map(voltL, 0, 9, 0, 255);
+
+  // RIGHT
+  analogWrite(ENA, 220);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+
+  // LEFT
+  analogWrite(ENB, 255);
+  digitalWrite(IN4, LOW);
+  digitalWrite(IN3, HIGH);
+  
+  Serial.print("Left: ");
+  Serial.println(RPM_L);Serial.println(VL);
+  Serial.print("Right: ");
+  Serial.println(RPM_R);Serial.println(VR);
+  
+  delay(T);
+}
+
+
+
+void EncoderRClicks(){
+  PulseCountR++;
+  
+}
+void EncoderLClicks(){
+  PulseCountL++;
+}
+
--- mobile_robot.ino
+++ mobile_robot.ino
@@ -1,60 +1,56 @@
-/* 
- * .. CONTROL GHOSTS ..  
- * 
- *
- *
- *
-*/
+// CONTROL GHOSTS
 
 #include <math.h>
 
-
-struct pos{
+struct pos {
   int x, y;
-  pos(int a, int b){
+  pos(int a, int b) {
     x = a;
     y = b;
   }
 };
 
-struct Robot{
+struct Robot {
   double x, y, theta;
-  Robot(){
+  Robot() {
     x = y = theta = 0;
   }
 };
 
 
 //define pins
-#define MOTOR1_PWM 0
-#define MOTOR2_PWM 0
-#define M1_dir1 0
-#define M1_dir2 0
-#define M2_dir1 0
-#define M2_dir2 0
-#define EncoderR 0
-#define EncoderL 0
+#define MOTORR_PWM 9
+#define MOTORL_PWM 10
+#define MR_dir1 4
+#define MR_dir2 5
+#define ML_dir1 6
+#define ML_dir2 7
+#define EncoderR 2
+#define EncoderL 3
 
-#define MAX_SAT 5
+#define MAX_SAT_R 7.92
+#define MAX_SAT_L 9
 
 #define Ts 50
 
-#define R 0.02
-#define L 0.15
-#define Cr 1
-#define Cl 1
-#define PULSES_PER_REV 12
+#define R 0.031
+#define L 0.115
+#define Cr 2.55
+#define Cl 2.25
+#define PULSES_PER_REV_R 55
+#define PULSES_PER_REV_L 30
 
 
-#define Kp 1
+#define Kp 10
 #define Ki 0
 #define Kd 0
 double integrator, lastError;
 double PulseCountR, PulseCountL;
 
-#define N_GOALS 5
-#define TOLERANCE 0.5
-pos goals[N_GOALS] = { pos(1,1), pos(1,2), pos(2,2), pos(3,2), pos(3,3) };
+#define N_GOALS 3
+#define TOLERANCE 0.05
+//pos goals[N_GOALS] = { pos(1, 1), pos(1, 2), pos(2, 2), pos(3, 2), pos(3, 3) };
+pos goals[N_GOALS] = { pos(1, 1), pos(1,0), pos(0,0)};
 int goal_idx;
 
 Robot robot;
@@ -65,9 +61,9 @@ Robot robot;
 
 //Function prototyping
 bool checkGoal(pos goal, Robot robot);  //check if the robot reaches the goal ?
-double theta_now(pos goal, Robot robot);  //Theta of the goal in respect of Robot coordinates 
-double PID_action(double error);  //PID controller: take the angle error and return omega 
-void applyVoltage(double VR, double VL);  //apply voltage to left and right motor - handel the direction of motor 
+double theta_now(pos goal, Robot robot);  //Theta of the goal in respect of Robot coordinates
+double PID_action(double error);  //PID controller: take the angle error and return omega
+void applyVoltage(double VR, double VL);  //apply voltage to left and right motor - handel the direction of motor
 void EncoderRClicks();  //
 void EncoderLClicks();  //
 
@@ -79,13 +75,13 @@ void setup() {
   Serial.begin(9600);
 
   //Motor and H bridge pins
-  pinMode(MOTOR1_PWM, OUTPUT);
-  pinMode(MOTOR2_PWM, OUTPUT);
-  pinMode(M1_dir1, OUTPUT);
-  pinMode(M1_dir2, OUTPUT);
-  pinMode(M2_dir1, OUTPUT);
-  pinMode(M2_dir2, OUTPUT);
-  
+  pinMode(MOTORR_PWM, OUTPUT);
+  pinMode(MOTORL_PWM, OUTPUT);
+  pinMode(MR_dir1, OUTPUT);
+  pinMode(MR_dir2, OUTPUT);
+  pinMode(ML_dir1, OUTPUT);
+  pinMode(ML_dir2, OUTPUT);
+
   //intialize encoders' pin and attach the interupt
   pinMode(EncoderR, INPUT);
   pinMode(EncoderL, INPUT);
@@ -98,36 +94,47 @@ void setup() {
 
 
 
-
-
-
-
-
-
 void loop() {
   double theta_error = theta_now(goals[goal_idx], robot);
   double w = PID_action(theta_error);
-  double v = 0.1;
-  double VL = (v-w*L/2)/(R*Cl);
-  double VR = (v+w*L/2)/(R*Cr);
+  double v = 0.4;
+  double VL = (v - w * L / 2) / (R * Cl);
+  double VR = (v + w * L / 2) / (R * Cr);
+  if(VL > MAX_SAT_L) VL = MAX_SAT_L;
+  else if(VL<-MAX_SAT_L) VL = -MAX_SAT_L;
+  if(VR > MAX_SAT_R) VR = MAX_SAT_R;
+  else if(VR<-MAX_SAT_R) VR = -MAX_SAT_R;
   applyVoltage(VR, VL);
 
-  double DR = 2*PI*R*PulseCountR/PULSES_PER_REV;
-  double DL = 2*PI*R*PulseCountL/PULSES_PER_REV;
-  double DC = (DR + DL)/2;
+  double DR = 2 * PI * R * PulseCountR / PULSES_PER_REV_R;
+  double DL = 2 * PI * R * PulseCountL / PULSES_PER_REV_L;
+  double DC = (DR + DL) / 2;
   PulseCountR = PulseCountL = 0;
 
   robot.x = robot.x + DC * cos(robot.theta);
   robot.y = robot.y + DC * sin(robot.theta);
-  robot.theta = robot.theta + (DR-DL)/L;
+  robot.theta = robot.theta + (DR - DL) / L;
 
   goal_idx += checkGoal(goals[goal_idx], robot);
-
-  if(goal_idx == N_GOALS){
-    return ;
+  
+  if (goal_idx == N_GOALS) {
+    analogWrite(MOTORR_PWM, 0);
+    analogWrite(MOTORL_PWM, 0);
+    while(1){}
   }
 
-
+  Serial.print("VL: ");
+  Serial.println(VL);
+  Serial.print("VR: ");
+  Serial.println(VR);
+  
+  Serial.print("X: ");
+  Serial.println(robot.x);
+  Serial.print("Y: ");
+  Serial.println(robot.y);
+  Serial.print("theta: ");
+  Serial.println(robot.theta);
+  
   delay(Ts);
 }
 
@@ -135,20 +142,13 @@ void loop() {
 
 
 
-
-
-
-
-
-
-
-double theta_now(pos goal, Robot robot){
-    return atan2(goal.y - robot.y, goal.x - robot.x) - robot.theta;
+double theta_now(pos goal, Robot robot) {
+  return atan2(goal.y - robot.y, goal.x - robot.x) - robot.theta;
 }
 
 
 
-double PID_action(double error){
+double PID_action(double error) {
   double u, derr;
 
   integrator += error;
@@ -161,40 +161,40 @@ double PID_action(double error){
 
 
 
-void applyVoltage(double VR, double VL){
-  analogWrite(MOTOR1_PWM, map(abs(VR), 0, MAX_SAT, 0, 255));
-  analogWrite(MOTOR2_PWM, map(abs(VL), 0, MAX_SAT, 0, 255));
-  if(VR>0){
-    digitalWrite(M1_dir1,HIGH);
-    digitalWrite(M1_dir2,LOW);
-  }else{
-    digitalWrite(M1_dir1,LOW);
-    digitalWrite(M1_dir2,HIGH);
+void applyVoltage(double VR, double VL) {
+  analogWrite(MOTORR_PWM, map(abs(VR), 0, MAX_SAT_R, 0, 220));
+  analogWrite(MOTORL_PWM, map(abs(VL), 0, MAX_SAT_L, 0, 255));
+  if (VR > 0) {
+    digitalWrite(MR_dir1, LOW);
+    digitalWrite(MR_dir2, HIGH);
+  } else {
+    digitalWrite(MR_dir1, HIGH);
+    digitalWrite(MR_dir2, LOW);
   }
 
-  if(VL>0){
-    digitalWrite(M2_dir1,HIGH);
-    digitalWrite(M2_dir2,LOW);
-  }else{
-    digitalWrite(M2_dir1,LOW);
-    digitalWrite(M2_dir2,HIGH);
+  if (VL > 0) {
+    digitalWrite(ML_dir1, HIGH);
+    digitalWrite(ML_dir2, LOW);
+  } else {
+    digitalWrite(ML_dir1, LOW);
+    digitalWrite(ML_dir2, HIGH);
   }
 }
 
 
 
-bool checkGoal(pos goal, Robot robot){
-    return ((abs(robot.x - goal.x) <= TOLERANCE) && (abs(robot.y - goal.y) <= TOLERANCE));
+bool checkGoal(pos goal, Robot robot) {
+  return ((abs(robot.x - goal.x) <= TOLERANCE) && (abs(robot.y - goal.y) <= TOLERANCE));
 }
 
 
 
 
-void EncoderRClicks(){
+void EncoderRClicks() {
   PulseCountR++;
-  
+
 }
-void EncoderLClicks(){
+void EncoderLClicks() {
   PulseCountL++;
 }
 
